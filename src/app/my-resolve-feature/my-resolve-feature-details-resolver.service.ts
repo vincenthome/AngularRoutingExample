3ce8a043e22795b0d95b0a486adbf91e3aba1049
Vincent Leung <vincentl@immonline.com>
2020-06-10 13:17:13 -0400

    added error handling model

--- src/app/my-resolve-feature/my-resolve-feature-details-resolver.service.ts
+++ src/app/my-resolve-feature/my-resolve-feature-details-resolver.service.ts
@@ -11,6 +11,13 @@ export class MyResolveFeatureDetailsResolverService implements Resolve<MyModel> 
 
   constructor(private dataService: MyResolveFeatureService, private router: Router) { }
 
+  // To support Error Handling, we can use MyModelResolved wrapper instead of MyModel
+  // allow us to pass error info to Component
+  // export interface MyModelResolved {
+  //   myModel: MyModel;
+  //   error?: any;
+  // }
+
   resolve(route: ActivatedRouteSnapshot): Observable<MyModel> | Promise<MyModel> | MyModel {
     const id = route.paramMap.get('id');
     return this.dataService.getItem(+id);
